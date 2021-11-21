8ffcf429bbdc10ecc97caa36bf7031c70ea5a9e3
avp1510 <avp1510@gmail.com>
2021-11-21 11:08:37 +0530

    Library renewd

--- app/src/main/java/com/example/lms/Books2.java
+++ app/src/main/java/com/example/lms/Books2.java
@@ -3,15 +3,69 @@ package com.example.lms;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.Collections;
+import java.util.Objects;
 
 public class Books2 extends AppCompatActivity {
+    Button button;
+
+    ListView mview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_books2);
+        Objects.requireNonNull(getSupportActionBar()).setTitle(Html.fromHtml("<font color='#ffffff'> Books</font>"));
+        mview=findViewById(R.id.irlog_view);
+        button=findViewById(R.id.show);
+        DatabaseHelper databaseHelper=new DatabaseHelper(Books2.this);
+
+
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Cursor res=databaseHelper.getbooks();
+
+                if(res.getCount()==0){
+                    Toast.makeText(Books2.this, "Library is empty.", Toast.LENGTH_SHORT).show();
+                }
+
+                StringBuffer buffer=new StringBuffer();
+                while (res.moveToNext()){
+                    buffer.append("BOOKID: "+res.getString(0)+", ");
+                    buffer.append("BOOKNAME: "+res.getString(1)+", ");
+                    buffer.append("ISBN: "+res.getString(2)+", ");
+                    buffer.append("PUBLISHER: "+res.getString(3)+", ");
+                    buffer.append("EDITION: "+res.getString(4)+", ");
+                    buffer.append("PAGES: "+res.getString(5)+", ");
+                    buffer.append("STATUS: "+res.getString(6)+"\n");
+                    buffer.append("\n");
+                    //newview.setAdapter(adapter);
+                }
+                ArrayAdapter adapter= new ArrayAdapter(Books2.this, android.R.layout.simple_list_item_1, Collections.singletonList(buffer));
+                mview.setAdapter(adapter);
+
+                /*
+                List<BookRows> everyone=databaseHelper.getEveryone();
+                ArrayAdapter customerArrayAdapter=new ArrayAdapter<BookRows>(Books.this, android.R.layout.simple_list_item_1,everyone);
+                mview.setAdapter(customerArrayAdapter);*/
+
+            }
+
+        });
+
+
     }
 
     public void btn_back(View view) {
